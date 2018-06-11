3717aa206ac5773c06af6598bb57eef9f58af793
EleMint <dylanschmidt0216@gmail.com>
2018-06-11 15:40:02 -0500

    Html layout complete, added javacript layout and data collection

--- scripts.js
+++ scripts.js
@@ -1,11 +1,40 @@
 
 
 function setAttributes(){
-	
+	initialHealth = parseFloat(document.getElementById('initialHealth').value);
+	addedHealth = parseFloat(document.getElementById('addedHealth').value);
+	attackWeapon = parseFloat(document.getElementById('attackWeapon').value);
+	attackStrength = parseFloat(document.getElementById('attackStrength').value);
+	defenceType = parseFloat(document.getElementById('defenceType').value);
+	defenceStrength = parseFloat(document.getElementById('defenceStrength').value);
+	initialHealthMultiplier = rollDie(initialHealth);
+	addedHealthMultiplier = rollDie(addedHealth);
+	attackWeaponMultiplier = rollDie(attackWeapon);
+	attackStrengthMultiplier = rollDie(attackStrength);
+	defenceTypeMultiplier = rollDie(defenceType);
+	defenceStrengthMultiplier = rollDie(defenceStrength);
+	calculateScores();
+}
+
+function calculateScores(){
+	calculateComputerScore();
+	calculatePlayerScore();
+}
+
+function calculateComputerScore(){
+
 }
 
 
+function calculatePlayerScore(){
 
+}
+
+
+//initial health: 10;
+//+ added health;
+//+ defencetype * defencestrength;
+//- computer attack;
 
 function rollDie(nSides){
 	return Math.floor((Math.random() * nSides) + 1);
@@ -22,6 +51,12 @@ function runDiceGame(){
 	let attackStrength;
 	let defenceType;
 	let defenceStrength;
+	let initialHealthMultiplier;
+	let addedHealthMultiplier;
+	let attackWeaponMultiplier;
+	let attackStrengthMultiplier;
+	let defenceTypeMultiplier;
+	let defenceStrengthMultiplier;
 
 
 
