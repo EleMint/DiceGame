70f3495558a1ab3dff9479dc49b9e1367d9d768c
EleMint <dylanschmidt0216@gmail.com>
2018-06-11 14:21:21 -0500

    Layout with rollDice function

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
-function submitName(){
-	let charName = document.getElementById('charName');
+//player roll 
+
+
+
+
+function promptUserForString(message){
+	return prompt(message);
+}
+
+function rollDie(nSides){
+	return Math.floor((Math.random() * nSides) + 1);
+}
+
+function removeHealth(hearts){
+
+
 }
 
 
 
-
-
-
-
-
-
-
 function runDiceGame(){
-
-
+	let health = 20;
+	//let charName = promptUserForString('Character Name:');
 
 
 
