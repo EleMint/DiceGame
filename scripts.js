7cbbcc44aed5bf35ad4d925bfa7e3d9d9d29bc02
EleMint <dylanschmidt0216@gmail.com>
2018-06-13 08:07:02 -0500

    Added calculateComputerTotalScore();, removed whitespace, and removed unused functions.

--- scripts.js
+++ scripts.js
@@ -14,33 +14,9 @@ function setAttributes(){
     let attackStrengthMultiplied = rollDie(attackStrength);
     let defenceTypeMultiplied = rollDie(defenceType);
     let defenceStrengthMultiplied = rollDie(defenceStrength);
-    let computerAttackWeaponMultiplied = getComputerAttackStrengthValue(20);
-    let computerAttackStrengthMultiplied = getComputerAttackWeaponValue(6);
+    let computerAttackWeaponMultiplied = rollDie(20);
+    let computerAttackStrengthMultiplied = rollDie(6);
     calculateComputerScore(initialHealthMultiplied, addedHealthMultiplied, defenceStrengthMultiplied, defenceTypeMultiplied, computerAttackStrengthMultiplied, computerAttackWeaponMultiplied, attackStrengthMultiplied, attackWeaponMultiplied);
-}
-
-function getComputerAttackStrengthValue(sides){
-	let output;
-	let counter = 0;
-	if(counter < 1){
-        counter++;
-        output = rollDie(sides);
-        return output;
-	} else {
-		return output;
-	}
-}
-
-function getComputerAttackWeaponValue(sides){
-	let output;
-	let counter = 0;
-	if(counter < 1){
-		counter++;
-		output = rollDie(sides);
-		return output;
-	} else {
-		return output;
-	}
 }
 
 function calculateComputerScore(initialHealthMultiplied, addedHealthMultiplied, defenceStrengthMultiplied, defenceTypeMultiplied, attackStrengthMultiplied, attackWeaponMultiplied){
@@ -52,14 +28,18 @@ function calculateComputerScore(initialHealthMultiplied, addedHealthMultiplied, 
     let computerAddedHealthMultiplied = rollDie(computerAddedHealth);
     let computerDefenceTypeMultiplied = rollDie(computerDefenceType);
     let computerDefenceStrengthMultiplied = rollDie(computerDefenceStrength);
-    let computerAttackWeaponMultiplied = getComputerAttackWeaponValue(12);
-    let computerAttackStrengthMultiplied = getComputerAttackStrengthValue(6);
-    let computerScore = Math.abs((computerInitialHealthMultiplied + computerAddedHealthMultiplied) * (computerDefenceStrengthMultiplied + computerDefenceTypeMultiplied) - (attackStrengthMultiplied + attackWeaponMultiplied));
-    let playerScore = Math.abs(calculatePlayerScore(initialHealthMultiplied, addedHealthMultiplied, defenceTypeMultiplied, defenceStrengthMultiplied,computerAttackWeaponMultiplied, computerAttackStrengthMultiplied));
+    let computerAttackWeaponMultiplied = rollDie(12);
+    let computerAttackStrengthMultiplied = rollDie(6);
+    let computerScore = calculateComputerTotalScore(computerInitialHealthMultiplied, computerAddedHealthMultiplied, computerDefenceStrengthMultiplied, computerDefenceTypeMultiplied, attackStrengthMultiplied, attackWeaponMultiplied);
+    let playerScore = calculatePlayerTotalScore(initialHealthMultiplied, addedHealthMultiplied, defenceTypeMultiplied, defenceStrengthMultiplied,computerAttackWeaponMultiplied, computerAttackStrengthMultiplied);
     decideWhoWins(playerScore, computerScore);
 }
 
-function calculatePlayerScore(initialHealthMultiplied, addedHealthMultiplied, defenceTypeMultiplied, defenceStrengthMultiplied, computerAttackWeaponMultiplied, computerAttackStrengthMultiplied){
+function calculateComputerTotalScore(compInitHealth, compAddHealth, compDefStrength, compDefType, playerAttStrength, playerAttWeapon){
+    return ((compInitHealth + compAddHealth) * (compDefStrength + compDefType)) - (playerAttStrength + playerAttWeapon);
+}
+
+function calculatePlayerTotalScore(initialHealthMultiplied, addedHealthMultiplied, defenceTypeMultiplied, defenceStrengthMultiplied, computerAttackWeaponMultiplied, computerAttackStrengthMultiplied){
 	return ((initialHealthMultiplied + addedHealthMultiplied) * (defenceStrengthMultiplied + defenceTypeMultiplied)) - (computerAttackStrengthMultiplied + computerAttackWeaponMultiplied);
 }
 
@@ -88,8 +68,6 @@ function checkIfSame(checkInitialValues) {
     let fourth = document.getElementById('attackStrength').value;
     let fifth = document.getElementById('defenceType').value;
     let sixth = document.getElementById('defenceStrength').value;
-
-
     let areSame = (first !== second && first !== third && first !== fourth && first !== fifth && first !== sixth && second !== third && second !== fourth && second !== fifth && second !== sixth && third !== fourth && third !== fifth && third !== sixth && fourth !== fifth && fourth !== sixth && fifth !== sixth);
 
     if(!areSame) {
@@ -98,13 +76,10 @@ function checkIfSame(checkInitialValues) {
         document.getElementById('setButton').disabled = false;
         clearInterval(checkInitialValues);
     }
-
-
 }
 
 function runDiceGame(){
     let count = 0;
     let checkInitialValues = setInterval(function(){checkIfSame(checkInitialValues); count++; if(count === 120){window.location.reload();}}, 1000);
-
 }
 runDiceGame();
